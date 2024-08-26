40b3d88d468a80422990a78bd047d3ff22ae2665
Oleg Smirnov <oleg31101996@gmail.com>
2024-08-26 13:23:47 +0400

    Add binary compatibility validator

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,15 @@
+import kotlinx.validation.ExperimentalBCVApi
+
 plugins {
   alias(libs.plugins.kotlin.mutliplatform) apply false
   alias(libs.plugins.kotlin.jvm) apply false
+  alias(libs.plugins.kotlin.binaryCompatibility)
+}
+
+apiValidation {
+  ignoredProjects += setOf("generator")
+  @OptIn(ExperimentalBCVApi::class)
+  klib {
+    enabled = true
+  }
 }
